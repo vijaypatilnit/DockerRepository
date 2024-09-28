083c082b7b918e94814d19957722adc24c76ad14
vijaypatilnit <vijaypatilnit@gmail.com>
2024-09-28 18:06:35 +0530

    dockerfile2

---
+++ SpringProject-2/src/main/java/com/example/controller/FrontAppController.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping
+public class FrontAppController {
+
+	
+	@GetMapping("/getUser")
+	public String getUserDetails() {
+		
+		return "Welcome to new orgnization";
+		
+	}
+}
